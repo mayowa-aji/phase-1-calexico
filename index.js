9b0d2ad4c23530c67539e8b7e3ca95e2e535a9b0
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-01-19 20:33:34 -0500

    complete code challenge

--- index.js
+++ index.js
@@ -1,77 +1,62 @@
-// Write your code here...
+//declare all global variables
+const container = document.querySelector('#container')
+const menu = document.querySelector('#menu')
+const menuItems = document.querySelector('#menu-items')
+const dish = document.querySelector('#dish')
+const dishImage = document.querySelector('#dish-image')
+const dishText = document.querySelector('#dish-text')
+const dishName = document.querySelector('#dish-name')
+const dishDescription = document.querySelector('#dish-description')
+const dishPrice = document.querySelector('#dish-price')
+const numberInCart = document.querySelector('#number-in-cart')
+const cartForm = document.querySelector('#cart-form')
+const cartAmount = document.querySelector('#cart-amount')
+const addAmount = parseInt(cartAmount.value || 0)
+const cartTotal = parseInt(numberInCart.textContent)
 
-/*CHALLENGE 2 */
-let FirstMenuItem;
 
-/*****CHALLENGE 1  */
-// Fetch all the menu items from
-//parse the JSON DATA file
-// loop though menu items array with forEach
-//within the loop, we will be going through each menu item
-//find the parent div and append the span to it
-
-let firstMenuItem;
-
-fetch('http://localhost:3002/menu')
-    .then(resp => resp.json())
-    .then(menuItems => menuItems.forEach(menuItem => {
-        firstMenuItem = menuItems[0]
-
-        const menuItemSpan = document.createElement('span')
-        menuItemSpan.textContent = menuItem.name
-
-        //challenge 3
-        menuItemSpan.addEventListener('click',(event) => {
-          renderMenuItem(menuItem)
-        })
-
-        document.querySelector('#menu-items').append(menuItemSpan)
-    }))
-    .then(() => {
-        // TODO add to screen
-        //Challenge 2
-        renderMenuItem(firstMenuItem)
-
-      //challenge 4
-      const cartForm = document.querySelector('#cart-form')
-      cartForm.addEventListener('submit',(event) => {
-        event.preventDefault()
-
-      const cartInput = document.querySelector('#cart-amount')
-      const numberInCart = document.querySelector('#number-in-cart')
-
-      const cartAmount = parseInt(numberInCart.textContent)
-      const addAmount = parseInt(cartInput.value)
-      numberInCart.textContent = cartAmount + addAmount
-      })
+//add fetch
+fetch('http://localhost:3000/menu')
+  .then(response => response.json())
+  .then((data) => {
+    renderMenuItems(data)
+    renderFirstItem(data[0])
     })
 
-function renderMenuItem(menuItem) {
-    const img = document.querySelector('#dish-image')
-    img.src = menuItem.image
-
-    const dishH3 = document.querySelector('#dish-name')
-    dishH3.textContent = menuItem.name
-
-    const dishDesc = document.querySelector('#dish-description')
-    dishDesc.textContent = menuItem.description
-
-    const dishPrice = document.querySelector('#dish-price')
-    dishPrice.textContent = menuItem.price
+//get all food items
+function renderMenuItems(menuItemsArray) {
+  menuItemsArray.forEach(data => renderMenuItem(data));
 }
-/*CHALLENGE 3 */
-function renderMenuItems(menuItem) {
-  const img = document.querySelector('#dish-image')
-  img.src = menuItem.image
 
-  const dishH3 = document.querySelector('#dish.name')
-  dishH3.textContent = menuItem.name
+//single food item
+function renderMenuItem(menuObj) {
+  const menuItem = document.createElement('span')
+  menuItem.textContent = menuObj.name
 
-  const dishDesc = document.querySelector('#dish-description')
-  dishDesc.textContent = menuItem.description
+  menuItem.addEventListener('click', () => {
+     renderFirstItem(menuObj)
+})
 
-  const dishPrice = document.querySelector('#dish-price')
-  dishPrice.textContent = menuItem.price
+  menuItems.append(menuItem)
+}
+
+function renderFirstItem(menuObj) {
+  dishImage.src = menuObj.image
+ dishName.textContent = menuObj.name
+  dishDescription.textContent = menuObj.description
+  dishPrice.textContent = `$${menuObj.price}`
 
 }
+
+function submitForm() {
+  cartForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    numberInCart.textContent = parseInt(numberInCart.textContent) + parseInt(cartAmount.value)
+
+
+  })
+}
+submitForm()
+
+
 
